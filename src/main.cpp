e59dc5aa824fdf508c5bcdcbb572dfe0c53f6820
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-09-12 18:14:25 +0200

    Reformat main()

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace cv;
 
 int main()
 {
-    /*const size_t boardSize=100;
+    const size_t boardSize=100;
     Board board(boardSize);
     board.fillWithRandom();
     BoardDisplayer displayer(10, 1);
@@ -14,44 +14,6 @@ int main()
     for(;;)
     {
         board();
-    }*/
-
-
-    const size_t boardSize=5;
-    Board board(boardSize);
-    uchar * const data = (uchar*)board.rawData();
-    memset((void *)data, 255, 25);
-    data[7] = 0;
-    data[12] = 0;
-    data[17] = 0;
-    //board.fillWithRandom();
-    BoardDisplayer displayer(50, 0);
-    board.addObserver(&displayer);
-    displayer.update(&board);
-    for(;;)
-    {
-        board.toNext();
     }
     return 0;
 }
-
-
-
-/**
- * TODO: Uni test
-*   const size_t boardSize=5;
-    Board board(boardSize);
-    uchar * const data = (uchar*)board.rawData();
-    memset((void *)data, 255, 25);
-    data[7] = 0;
-    data[12] = 0;
-    data[17] = 0;
-    //board.fillWithRandom();
-    BoardDisplayer displayer(50, 0);
-    board.addObserver(&displayer);
-    displayer.update(&board);
-    for(;;)
-    {
-        board.toNext();
-    }
-*/
