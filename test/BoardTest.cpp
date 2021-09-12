1b2786bc911451414b5da6248d56e4ea78d0b5b7
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-09-12 19:53:33 +0200

    Add unit tests

--- test/BoardTest.cpp
+++ test/BoardTest.cpp
@@ -1,6 +1,18 @@
 #include "BoardTest.h"
 
-TEST_F(BoardTest, ThreeAlignedPixels) {
+TEST_F(BoardTest, SinglePixel)
+{
+    Board board(5);
+    board.pauseSimulation(false);
+    unsigned char* data = (unsigned char*)board.rawData();
+    memset((void *)data, 255, 25);
+    board[2][2] = 0;
+    board();
+    EXPECT_EQ((int)board[2][2], 255);
+}
+
+TEST_F(BoardTest, ThreeAlignedPixels)
+{
     Board board(5);
     board.pauseSimulation(false);
     unsigned char* data = (unsigned char*)board.rawData();
@@ -18,4 +30,32 @@ TEST_F(BoardTest, ThreeAlignedPixels) {
     EXPECT_EQ((int)board[1][2], 0);
     EXPECT_EQ((int)board[2][2], 0);
     EXPECT_EQ((int)board[3][2], 0);
+}
+
+TEST_F(BoardTest, PausedBoard)
+{
+    Board board(5);
+    board.pauseSimulation(true);
+    unsigned char* data = (unsigned char*)board.rawData();
+    memset((void *)data, 255, 25);
+    board[2][2] = 0;
+    board();
+    EXPECT_EQ((int)board[2][2], 0);
+}
+
+TEST_F(BoardTest, Square)
+{
+    Board board(5);
+    board.pauseSimulation(true);
+    unsigned char* data = (unsigned char*)board.rawData();
+    memset((void *)data, 255, 25);
+    board[1][1] = 0;
+    board[1][2] = 0;
+    board[2][1] = 0;
+    board[2][2] = 0;
+    board();
+    EXPECT_EQ((int)board[1][1], 0);
+    EXPECT_EQ((int)board[1][2], 0);
+    EXPECT_EQ((int)board[2][1], 0);
+    EXPECT_EQ((int)board[2][2], 0);
 }
